01c776f4a1e87d080a4e409e6777cc999e06a70d
Michael Ndigi <38488451+Ndigbiggs@users.noreply.github.com>
2021-11-23 00:22:51 +0300

    Add files via upload

---
+++ Do while loop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i = 11;
+	do{
+  
+      if(i % 2 == 0){
+   
+         cout <<i<< " ";
+      }
+  
+  
+     i++;
+	}
+	
+while(i <= 50);
+
+	return 0;	
+}
---
+++ While loop assignment.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+    int i=11;
+ 
+   while( i <= 28){
+  
+      if(i % 2 == 0){
+   
+         cout <<i<< " ";
+      }
+  
+  
+     i++;
+ }
+ 
+ return 0;
+}
